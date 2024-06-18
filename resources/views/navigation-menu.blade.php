3e244cfeac858d270b01a8e8af72efb7986a3c86
HanThamarat <HanThamarat@gmail.com>
2024-06-18 20:32:30 +0700

    update style nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-primary border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-28">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -78,13 +78,28 @@
                                     <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
                             @else
+                                @php
+                                    $text = Auth::user()->name;
+                                    $textSplit = explode(" ", $text);
+                                    if (empty($textSplit[1])) {
+                                        $substring = substr($textSplit[0], 0, 1);
+                                    } else {
+                                        $sub01 = substr($textSplit[0], 0, 1);
+                                        $sub02 = substr($textSplit[1], 0, 1);
+                                        $substring = $sub01.$sub02;
+                                    }
+                                @endphp
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border-white text-sm leading-4 font-medium rounded-md text-white bg-primary transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-
-                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
+                                       <div class="mr-2 h-[40px] w-[40px] rounded-full bg-red-600 text-white flex justify-center items-center border-2 border-red-400">
+                                            <span class="text-[16px] font-medium">{{ $substring }}</span>
+                                       </div>
+                                        <div class="flex">
+                                            <span>{{ Auth::user()->name }}</span>
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </div>
                                     </button>
                                 </span>
                             @endif
